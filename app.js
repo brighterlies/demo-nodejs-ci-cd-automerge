4487c77cec37640d67881fc09204ff3a985c7d15
Marianne Infantas <marianneinfantas@gmail.com>
2025-07-14 23:36:02 -0400

    feat: test dev

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('App funcionando con CI/CD desde GitHub Actions!'));
+app.get('/', (req, res) => res.send('🚀 App funcionando con CI/CD desde GitHub Actions! 🚀'));
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en puerto 3000');
